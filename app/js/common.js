b46fd6430b0946e553c195ebe0de3179304ad543
Viktoria <zaharova181114@gmail.com>
2021-05-21 03:06:51 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,50 @@
+$('.dropdown-toggle').on('click', function (e) {
+    e.preventDefault();
+    $(this).siblings('.dropdown-menu').fadeToggle();
+});
+
+//плавный скролл
+$(document).ready(function () {
+    $('.go_to').click(function (e) {
+        e.preventDefault();
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length !== 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 500);
+        }
+        $('.mobile-menu').fadeOut();
+
+        return false;
+    });
+});
+//плавный скролл end
+
+$('.main-slider').slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: true
+});
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeIn();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() > 768) {
+        $('.products-slider').slick({
+            slidesToShow: 2,
+            arrows: false,
+            dots: true,
+            infinite: false,
+        });
+    } else {
+        $(".products-slider.slick-initialized").slick("unslick");
+    }
+});
+// slick active
